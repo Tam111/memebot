4c796864a03f0cf2b30e9366e9bd5b2fdc19cff3
Tam111 <thomasmac20@gmail.com>
2019-09-09 19:16:28 +1000

    setup

--- index.js.js
+++ index.js.js
@@ -1,4 +1,4 @@
-const botconfig =  require("./botconfig.json");
+﻿const botconfig =  require("./botconfig.json");
 const Discord = require("discord.js");
 var express = require ("express");
 var app = express();
@@ -6,8 +6,8 @@ var port = process.env.PORT || 3000
 const bot = new Discord.Client({disableEveryone: true})
 
 bot.on("ready", async () => {
-console.log('MythBot is online!');
-bot.user.setActivity("Use m!help");
+console.log('MemeBot is online!');
+bot.user.setActivity("Use r!help");
 });
 
 bot.on("message", async message => {
@@ -20,19 +20,15 @@ let cmd = messageArray[0];
 let args = messageArray.slice(1);
 	
 let flip = ["Heads", "Tails"];	
-	
-let fortunes = ["“The Way Get Started Is To Quit Talking And Begin Doing.” – Walt Disney", "“The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.” – Winston Churchill", "Solitude has its own very strange beauty to it. Liv Tyler", "Biologically speaking, if something bites you, it is more likely to be female. Desmond Morris","If everyone is thinking alike, then somebody isn’t thinking. George S. Patton"];
-	
-let replies = ["Hello", "Hi", "Hey"];
-	
-let trainerTips = ["If you're in an area you know you can't beat, you can always follow Snake's moves and stealth through the area", "Remember to always save before you fight a legendary Pokemon!", "Always carry a few Pokeballs around, you never know if you might find a shiny!","Pokemon will gain less experience when battling an enemy of a lower level", "Carry Heavy Balls to legendary fights, most legendaries are heavy so it's the best chance at catching them without a Master Ball!","Ice is a very frail type, never try to stall with one unless it has absurd defenses or a certain useful ability","Be aware of your Pokemon's strengths and weaknesses to make the best strategy you can", "You moves with a STAB bonus when you can", "Give all your Pokemon items. Even if it's only a berry","Always keep check of your team's type composition, make sure no one type can wipe your whole team out!", "Try not to double up on type coverage in your team"]	
+let replies = ["Hello!", "Hi!", "Hey!"]
 
-let doggoPictures = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI5AcefHo-BDNJiijvDZqEKqpNoebFWBkpH9YTkubbXxgqcC-L", "https://pbs.twimg.com/profile_images/828073361397932032/eKTigt-2_400x400.jpg", "https://data.whicdn.com/images/293182173/superthumb.jpg?t=1501286905", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdD-L8LX_brly3nAYnH_ZPw4g7ALxuQcb9injwk2AEHdlXpNxw-g", "https://i.ytimg.com/vi/2Bm4H33xC9s/maxresdefault.jpg", "https://i.ytimg.com/vi/alJHDHo9nMA/hqdefault.jpg", "https://pbs.twimg.com/profile_images/972443397741932544/ZYjQQfmR.jpg"]
+let doggoPictures = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI5AcefHo-BDNJiijvDZqEKqpNoebFWBkpH9YTkubbXxgqcC-L", "https://pbs.twimg.com/profile_images/828073361397932032/eKTigt-2_400x400.jpg", "https://data.whicdn.com/images/293182173/superthumb.jpg?t=1501286905", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdD-L8LX_brly3nAYnH_ZPw4g7ALxuQcb9injwk2AEHdlXpNxw-g", "https://i.ytimg.com/vi/2Bm4H33xC9s/maxresdefault.jpg", "https://i.ytimg.com/vi/alJHDHo9nMA/hqdefault.jpg", "https://pbs.twimg.com/profile_images/972443397741932544/ZYjQQfmR.jpg","https://i.imgur.com/mUIQRVy.jpg","https://i.redd.it/9y1tprwmbnb01.png","https://i.redd.it/kruwv92y42911.jpg","https://hips.hearstapps.com/ghk.h-cdn.co/assets/18/08/3200x1600/landscape-1519236394-rudy-barclay.jpg?resize=480:*"]
+
+let rwbyPictures = ["https://media.discordapp.net/attachments/438528753581162518/620528214590226432/image1.jpg?width=473&height=473", "https://cdn.discordapp.com/attachments/438528753581162518/620528214590226433/image3.jpg", "https://cdn.discordapp.com/attachments/438528753581162518/620528215269572608/image0.jpg", "https://cdn.discordapp.com/attachments/438528753581162518/620528215269572609/image2.jpg","https://cdn.discordapp.com/attachments/438528753581162518/620531641713950730/image1.jpg","https://cdn.discordapp.com/attachments/438528753581162518/620531641713950732/image0.jpg"]
 
 let doggoPicture = Math.floor((Math.random() * doggoPictures.length));
-let helloResponse = Math.floor((Math.random() * replies.length));
-let yourFortune = Math.floor((Math.random() * fortunes.length));	
-let trainerTip = Math.floor((Math.random() * trainerTips.length));	
+let rwbyPicture = Math.floor((Math.random() * rwbyPictures.length));
+let helloResponse = Math.floor((Math.random() * replies.length));	
 let flipACoin = Math.floor((Math.random() * flip.length));	
 	
 if(cmd === `${prefix}hello`){	
@@ -40,164 +36,39 @@ return message.channel.send(replies[helloResponse]);
 	
 }
 
-if(cmd === `${prefix}lady`){
-return message.channel.send("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMHSiZdIHrbwqUgQ_DKtUgK-Yzxc7hv9Doknm0UnjI7dAkQInh");
-	
-}
 
-if(cmd === `${prefix}amma mia`){
-return message.channel.send("https://media.discordapp.net/attachments/446173059418685452/460761477142282240/rsz_super-mario-odyssey-shocked-mario.png?width=400&height=230");
-	
-}	
-
-if(cmd === `${prefix}daily`){
-return message.channel.send("You received your 200 dai... wait wrong bot");
-	
-}
-	
-if(cmd === `${prefix}axmoefoe`){
-return message.channel.send("Finally caught up with maxmoefoe. He spent 15 months promising us a main channel video but then just started opening pokemon cards and dating his dog. Great entertainer he is");
-	
-}
-
-if(cmd === `${prefix}bunny`){
-return message.channel.send("https://media.discordapp.net/attachments/446173059418685452/461053193460252672/skibunne2.gif");
-	
-}	
-	
-if(cmd === `${prefix}picture`){
-return message.channel.send("My profile picture was created by Chicoatl!");
-	
-}
-	
-if(cmd === `${prefix}complain`){
-return message.channel.send("https://media.discordapp.net/attachments/461054056438431744/461103052447940624/spite.png?width=379&height=301");
-	
-}
-	
-if(cmd === `${prefix}om`){
-return message.channel.send("https://youtu.be/WfYyBp4Ln2s");
-	
-}
-	
-if(cmd === `${prefix}ban`){
-return message.channel.send("You are now banned");
-	
-}
-	
-if(cmd === `${prefix}oof`){
-return message.channel.send("https://www.youtube.com/watch?v=1Ixj2RV4dac");
-	
-}
-	
-if(cmd === `${prefix}bait`){
-return message.channel.send("https://media.discordapp.net/attachments/461055594351296512/464749225045917696/800.png?width=300&height=300");
-	
-}
-	
-if(cmd === `${prefix}thisissosad`){
-return message.channel.send("https://youtu.be/-tsBp26NXz0");
-	
-}
-		
-	
-if(cmd === `${prefix}unk`){
-return message.channel.send("https://media.discordapp.net/attachments/461054056438431744/466880028848685056/triggered.gif?width=226&height=301");
-	
-}
-	
-if(cmd === `${prefix}lifeisfun`){
-return message.channel.send("https://youtu.be/CAb_bCtKuXg");
-	
-}	
-	
-if(cmd === `${prefix}r rogers`){
-return message.channel.send("https://cdn.discordapp.com/attachments/461055594351296512/474518642508169226/d71.png");
-	
-}
-	
-if(cmd === `${prefix}feed sally`){
-return message.channel.send("You feed Sally some food.");
-	
-}
-	
-if(cmd === `${prefix}fortune`){
-return message.channel.send(fortunes[yourFortune]);
-	
-}	
-	
-if(cmd === `${prefix}trainertip`){
-return message.channel.send(trainerTips[trainerTip]);
-	
-}	
 	
 if(cmd === `${prefix}coin`){	
 return message.channel.send(flip[flipACoin]);	
 
 }
 
-if(cmd === `${prefix}fbi`){	
-return message.channel.send("FBI open up!");	
+if(cmd === `${prefix}rwby`){	
+return message.channel.send(rwbyPictures[rwbyPicture]);	
 
 }
-	
+
 if(cmd === `${prefix}doggo`){
 return message.channel.send(doggoPictures[doggoPicture]);
 	
-}		
+}
+
 	
 if(cmd === `${prefix}help`){	
 	
 let botembed = new Discord.RichEmbed()
-.setDescription("MythBot help menu page 1")
+.setDescription("MemeBot help menu page 1")
 .setColor("0000FF")
-.addField("m!help 2","The second page of commands")
-.addField ("m!hello","To say hello")
-.addField ("m!lady","Self explanitory")
-.addField ("m!daily","To recieve your daily tokens?")
-.addField ("m!amma mia","For a spicy meat-a ball")
-.addField ("m!axmoefoe","For a spicy meme")
-.addField ("m!bunny","For a cool bunny")
-.addField ("m!picture","For information on the profile picture")
-.addField ("m!coin","Flip a coin")
+
+.addField ("r!hello","To say hello")
+.addField ("r!rwby", "For a rwby meme")
+.addField ("r!coin","Flip a coin")
+.addField ("r!doggo", "For cute doggo")
 
 return message.channel.send(botembed);
 }
 	
-if(cmd === `${prefix}help 2`){	
-	
-let botembed = new Discord.RichEmbed()
-.setDescription("MythBot help menu page 2")
-.setColor("0000FF")
-.addField("m!help","The first page of commands")
-.addField ("m!help 3","The third page of commands")
-.addField ("m!complain","For hardcore complainers")
-.addField ("m!om","More memes by Dirtface")
-.addField ("m!daily","To recieve your daily tokens?")
-.addField ("m!ban","To totally legit ban someone")
-.addField ("m!oof","Oof")
-.addField ("m!bait","To deal with trolls")
-.addField ("m!thisissosad","More like this is so sad this bot has come to this")
 
-return message.channel.send(botembed);
-}	
-	
-if(cmd === `${prefix}help 3`){	
-	
-let botembed = new Discord.RichEmbed()
-.setDescription("MythBot help menu page 3")
-.setColor("0000FF")
-.addField("m!help 2","The second page of commands")
-.addField ("m!unk","Munk")
-.addField ("m!lifeisfun","More expresso, less depresso")
-.addField ("m!r rogers","I should make a seperate bot")
-.addField ("m!feed sally","Only used on Myths server")
-.addField ("m!fortune","For a quote that makes you think")
-.addField ("m!trainertip","To get advice in being a good trainer!")
-.addField ("m!doggo", "Pictures of cute doggos")
-
-return message.channel.send(botembed);
-}	
 	
 });
 
